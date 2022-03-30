5b04f126079a83776634fc73c0198eabad33008b
voyager <ardakmese@gmail.com>
2022-03-30 11:04:35 +0300

    second

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+# Operating Systems Homework
+# Date: 24.03.2022
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+import os
+import time
+
+class Matrix:
+    def __init__(self, x, y):
+        self.mX = x
+        self.mY = y
+        self.mList = {}
+        self.fill()
+
+    def fill(self):
+        for x in range(len(self.mX)):
+            list = {}
+            for y  in range(len(self.mY)):
+                list.append(range.uniform(0,1))
+            self.mList.append(list)
+        print(self.mList)
+
+# input two matrices of size n x m
+start_time = time.time()
+matrix1 = [[12, 7, 3],
+           [4, 5, 6],
+           [7, 8, 9]]
+matrix2 = [[5, 8, 1],
+           [6, 7, 3],
+           [4, 5, 9]]
+
+res = [[0 for x in range(3)] for y in range(3)]
+
+def multiply():
+    # explicit for loops
+    for i in range(len(matrix1)):
+        for j in range(len(matrix2[0])):
+            for k in range(len(matrix2)):
+                # resulted matrix
+                res[i][j] += matrix1[i][k] * matrix2[k][j]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+multiply()
+print("--- %s seconds ---" % (time.time() - start_time))
+print(res)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        firstX = input("Please enter first")
+        firstMatrix = Matrix(4,5)
