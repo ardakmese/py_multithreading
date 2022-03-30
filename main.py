3793e05e5985d189e90e87590d404485e6fc4033
voyager <ardakmese@gmail.com>
2022-03-30 11:40:49 +0300

    third

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import os
 import time
 
 class Matrix:
-    def __init__(self, x, y):
+    def __init__(self, x, y, threading = 0):
         self.mX = x
         self.mY = y
         self.mList = []
         self.fill()
+        self.mThread = threading
 
     def fill(self):
         for x in range(self.mX):
@@ -20,47 +21,54 @@ class Matrix:
             self.mList.append(list)
         print(self.mList)
 
-    def multiply(self,matrix):
+    def multiply(self, matrix):
         start_time = time.time()
+        res = [[0 for x in range(self.mX)] for y in range(matrix.mY)]
         for i in range(self.mX):
             for j in range(matrix.mY):
                 for k in range(matrix.mX):
-                    # resulted matrix
-                    res[i][j] += matrix1[i][k] * matrix2[k][j]
+                    res[i][j] += self.mList[i][k] * matrix.mList[k][j]
         print(res)
+        print("--- %s seconds ---" % (time.time() - start_time))
 
 # input two matrices of size n x m
-matrix1 = [[12, 7, 3],
-           [4, 5, 6],
-           [7, 8, 9]]
-matrix2 = [[5, 8, 1],
-           [6, 7, 3],
-           [4, 5, 9]]
+# res = [[0 for x in range(3)] for y in range(3)]
 
-res = [[0 for x in range(3)] for y in range(3)]
+def setMatrix():
+    global firstX, firstY, secondX, secondY
+    global firstMatrix, secondMatrix
+    firstX = input("Please enter first matrix's column size: ")
+    firstY = input("Please enter first matrix's row size: ")
+    secondX = input("Please enter second matrix's column size: ")
+    secondY = input("Please enter second matrix's row size: ")
 
-def multiply():
-    # explicit for loops
-    for i in range(len(matrix1)):
-        for j in range(len(matrix2[0])):
-            for k in range(len(matrix2)):
-                # resulted matrix
-                res[i][j] += matrix1[i][k] * matrix2[k][j]
+    if not (firstX.isdigit() or firstY.isdigit() or secondX.isdigit() or secondY.isdigit()):
+        print("Please enter inputs correctly!")
+        setMatrix()
+    elif not (firstX == secondX and firstY == secondY) or (
+            firstX != secondX and firstX == secondY):  # necessary for multiplying two matrixes to check dimensions!
+        print("Please enter dimensions correctly for multiplying!")
+        setMatrix()
+
+def multiplyWithThreading():
+    setMatrix()
+    threadSize = input("Please enter thread size to use while multiplying: ")
+    if not threadSize.isdigit():
+        print("Input is not correct!")
+        multiplyWithThreading()
+
+    firstMatrix = Matrix(int(firstX), int(firstY), int(threadSize))
+    secondMatrix = Matrix(int(secondX), int(secondY))
+    firstMatrix.multiply(secondMatrix)
+
 
 if __name__ == '__main__':
     while True:
-        firstX = input("Please enter first matrix's column size: ")
-        firstY = input("Please enter first matrix's row size: ")
-        secondX = input("Please enter second matrix's column size: ")
-        secondY = input("Please enter second matrix's row size: ")
+        setMatrix()
+        firstMatrix = Matrix(int(firstX), int(firstY))
+        secondMatrix = Matrix(int(secondX), int(secondY))
+        firstMatrix.multiply(secondMatrix)
 
-        if not (firstX.isdigit() or firstY.isdigit() or secondX.isdigit() or secondY.isdigit()):
-            print("Please enter inputs correctly!")
-            continue
-        elif not (firstX == secondX and firstY == secondY) or (firstX != secondX and firstX == secondY):  # necessary for multiplying two matrixes to check dimensions!
-            print("Please enter dimensions correctly for multiplying!")
-            continue
+        multiplyWithThreading()
 
-        firstMatrix = Matrix(int(firstX),int(firstY))
-        secondMatrix = Matrix(int(secondX),int(secondY))
 
