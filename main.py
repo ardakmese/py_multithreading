da75d3ef0641afe0854c7c4026e93e13073a2a09
voyager <ardakmese@gmail.com>
2022-03-30 11:21:02 +0300

    second

--- main.py
+++ main.py
@@ -9,19 +9,27 @@ class Matrix:
     def __init__(self, x, y):
         self.mX = x
         self.mY = y
-        self.mList = {}
+        self.mList = []
         self.fill()
 
     def fill(self):
-        for x in range(len(self.mX)):
-            list = {}
-            for y  in range(len(self.mY)):
-                list.append(range.uniform(0,1))
+        for x in range(self.mX):
+            list = []
+            for y  in range(self.mY):
+                list.append(random.uniform(0,1))
             self.mList.append(list)
         print(self.mList)
 
+    def multiply(self,matrix):
+        start_time = time.time()
+        for i in range(self.mX):
+            for j in range(matrix.mY):
+                for k in range(matrix.mX):
+                    # resulted matrix
+                    res[i][j] += matrix1[i][k] * matrix2[k][j]
+        print(res)
+
 # input two matrices of size n x m
-start_time = time.time()
 matrix1 = [[12, 7, 3],
            [4, 5, 6],
            [7, 8, 9]]
@@ -39,12 +47,20 @@ def multiply():
                 # resulted matrix
                 res[i][j] += matrix1[i][k] * matrix2[k][j]
 
-
-multiply()
-print("--- %s seconds ---" % (time.time() - start_time))
-print(res)
-
 if __name__ == '__main__':
     while True:
-        firstX = input("Please enter first")
-        firstMatrix = Matrix(4,5)
+        firstX = input("Please enter first matrix's column size: ")
+        firstY = input("Please enter first matrix's row size: ")
+        secondX = input("Please enter second matrix's column size: ")
+        secondY = input("Please enter second matrix's row size: ")
+
+        if not (firstX.isdigit() or firstY.isdigit() or secondX.isdigit() or secondY.isdigit()):
+            print("Please enter inputs correctly!")
+            continue
+        elif not (firstX == secondX and firstY == secondY) or (firstX != secondX and firstX == secondY):  # necessary for multiplying two matrixes to check dimensions!
+            print("Please enter dimensions correctly for multiplying!")
+            continue
+
+        firstMatrix = Matrix(int(firstX),int(firstY))
+        secondMatrix = Matrix(int(secondX),int(secondY))
+
